3a8cc39aa5c7588db4f698a6dbd810493747a4ea
Michael Pavkovic <mpavkovic688@gmail.com>
2017-10-28 23:08:16 -0400

    remove node_modules directory

--- server.js
+++ server.js
@@ -1,15 +1,50 @@
 const express = require('express')
 const app = express()
 const download = require('./download.js')
+const youtube = require('./youtube.js')
 
-app.get('/', function(req, res) {
+app.get('/download', function(req, res) {
+    let searchQuery = req.query.searchQuery
     let videoId = req.query.videoId
 
-    download.getAudio(videoId, function(path) {
-        res.contentType = 'audio/ogg'
-
-        res.download(path)
-    })
+    if (searchQuery == undefined) {
+        if (videoId == undefined) {
+            error = {
+                error: {
+                    reason: 'no-data-provided'
+                }
+            }
+            res.send(error)
+            return
+        } else {
+            download.getAudio(videoId, (path) => {
+                res.contentType = 'audio/ogg'
+                res.download(path)
+                // res.send({song: {
+                //     artist: 'artist',
+                //     title: 'title',
+                //     album: 'album',
+                //     genre: 'genre'
+                // }})
+            })
+        }
+    } else {
+        youtube.getVideoId(searchQuery, (it) => {
+            download.getAudio(it, (path) => {
+                res.contentType = 'audio/ogg'
+                res.download(path)
+                // res.send({song: {
+                //     artist: 'artist',
+                //     title: 'title',
+                //     album: 'album',
+                //     genre: 'genre'
+                // }})
+            })
+        })
+    }
 })
 
+app.get('/get-next', function(req, res) {
+    
+})
 app.listen(3000)
